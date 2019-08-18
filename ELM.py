6081d3ecf8b71989b58e92f4bb4effea34051a47
victorskg <victorskg@outlook.com>
2019-08-18 16:06:01 -0300

    Implementação da ELM

---
+++ ELM.py
@@ -0,0 +1,61 @@
+import math
+import numpy as np
+
+class ELM(object):
+    def __init__ (self, hid_num, out_num):
+        self.hid_num = hid_num
+        self.out_num = out_num
+
+    def train(self, dataset, att):
+        X, y = np.array([self.inputs(d, att) for d in dataset]), [self.expected(d, att) for d in dataset]
+
+        X = self.add_bias(X)
+
+        np.random.seed()
+        self.W = np.random.uniform(-1., 1.,
+                                   (self.hid_num, X.shape[1]))
+        
+        _H = np.linalg.pinv(self.sigmoid(np.dot(self.W, X.T)))
+
+        self.beta = np.dot(_H.T, y)
+        
+        return self
+
+    def test(self, test_data, att):
+        hits = 0
+        X, d = np.array([self.inputs(d, att) for d in test_data]), [self.expected(d, att) for d in test_data]
+        
+        _H = self.sigmoid(np.dot(self.W, self.add_bias(X).T))
+        
+        y = np.dot(_H.T, self.beta)
+        y_label = [self.predict(out) for out in y]
+        
+        for i,j in zip(y_label, d):
+            hits = hits + 1 if np.array_equal(i, j) else hits
+        
+        return (hits / len(test_data)) * 100
+
+    @staticmethod
+    def sigmoid(x):
+        sigmoid_range = 34.538776394910684
+        x = np.clip(x, -sigmoid_range, sigmoid_range)
+        return 1.0 / (1.0 + np.exp(-x))
+
+    @staticmethod
+    def add_bias(X):
+        return np.c_[X, np.ones(X.shape[0])]
+
+    @staticmethod
+    def inputs(d, att):
+        selected_inputs = [d[att[i]] for i in range(len(att))]
+        return selected_inputs
+
+    @staticmethod
+    def expected(d, att):
+        expected = np.array([d[len(d)-1]]) if (isinstance(d[len(d)-1], np.floating)) else np.array(list(d[len(d)-1])).astype(np.int)
+        return expected
+
+    @staticmethod
+    def predict(outputs):
+        predict = [1 if output == np.amax(outputs) else 0 for output in outputs]
+        return predict
--- main.py
+++ main.py
@@ -1,17 +1,24 @@
 import numpy as np
+from ELM import ELM
 from MLP import MLP
 import DataUtils as ut
 
 def main():
     #run_iris()
     #run_vertebral()
-    run_dermatology()
+    #run_dermatology()
     #run_artificial()
+
+    #run_elm_iris()
+    #run_elm_vertebral()
+    #run_elm_dermatology()
 
 def run_iris():
     acs = []
     att = [0, 1, 2, 3]
     d = ut.get_iris_data().to_numpy()
+
+    print('======== MLP IRIS ========')
     
     for i in range(20):
         mlp = MLP(4, 0.1, 500, 6, 3)
@@ -25,6 +32,8 @@ def run_vertebral():
     acs = []
     att = [0, 1, 2, 3, 4, 5]
     d = ut.get_column_data().to_numpy()
+
+    print('======== MLP VERTEBRAL ========')
 
     for i in range(20):
         mlp = MLP(6, 0.1, 500, 12, 3)
@@ -40,6 +49,52 @@ def run_dermatology():
     mlp = MLP(34, 0.1, 500, 12, 6)
     print(realization(mlp, d, att))
 
+def run_elm_iris():
+    acs = []
+    att = [0, 1, 2, 3]
+    d = ut.get_iris_data().to_numpy()
+
+    print('======== ELM IRIS ========')
+
+    for i in range(20):
+        elm = ELM(6, 3)
+        acs.append(realization_elm(elm, d, att))
+
+        print('Realização: {0}, Acurácia: {1}%'.format(i, acs[i]))
+    
+    print('Acurácia após 20 realizações: {0}%, Desvio padrão: {1}'.format(calc_accuracy(acs), np.std(acs)))
+
+def run_elm_vertebral():
+    acs = []
+    att = [0, 1, 2, 3, 4, 5]
+    d = ut.get_column_data().to_numpy()
+
+    print('======== ELM VERTEBRAL ========')
+
+    for i in range(20):
+        elm = ELM(8, 3)
+        acs.append(realization_elm(elm, d, att))
+
+        print('Realização: {0}, Acurácia: {1}%'.format(i, acs[i]))
+    
+    print('Acurácia após 20 realizações: {0}%, Desvio padrão: {1}'.format(calc_accuracy(acs), np.std(acs)))
+    
+
+def run_elm_dermatology():
+    acs = []
+    att = [x for x in range(34)]
+    d = ut.get_dermatology().to_numpy()
+    
+    print('======== ELM DERMATOLOGY ========')
+    
+    for i in range(20):
+        elm = ELM(18, 6)
+        acs.append(realization_elm(elm, d, att))
+
+        print('Realização: {0}, Acurácia: {1}%'.format(i, acs[i]))
+    
+    print('Acurácia após 20 realizações: {0}%, Desvio padrão: {1}'.format(calc_accuracy(acs), np.std(acs)))
+
 def run_artificial():
     att = [0]
     mlp = MLP(1, 0.1, 500, 1, 1)
@@ -54,6 +109,14 @@ def realization(mlp, d, att):
     mlp.train(train_data, att)
     
     return mlp.test(test_data, att)
+
+def realization_elm(elm, d, att):
+    np.random.shuffle(d)
+    qt_training = int(0.8 * len(d))
+    train_data, test_data = d[:qt_training], d[qt_training:]
+    elm.train(train_data, att)
+    
+    return elm.test(test_data, att)
 
 def realization_regression(mlp, d, att):
     np.random.shuffle(d)
